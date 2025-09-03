4a0bdd0e98376134f2cb51e1c6090886e96a3bb7
moha6959 <mohamedekaden576@gmail.com>
2025-09-03 10:02:56 +0300

    test for phpmailer

---
+++ Global/classes.php
@@ -0,0 +1,12 @@
+<?php
+// Creating a class
+class classes {
+    public function greet() {
+        return "<h1>Hello, ICS  B!</h1>";
+    }
+
+    public function today() {
+        return "<p>Today is " . date("l") . "</p>";
+    }
+}
+?>
